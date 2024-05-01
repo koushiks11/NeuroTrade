203de957283360d7d395de78f7f877671765a326
koushiks11 <koushiks.is20@bmsce.ac.in>
2024-05-01 15:42:34 +0530

    Deployment Requirements

--- pages/Our Insights.py
+++ pages/Our Insights.py
@@ -15,7 +15,7 @@ import urllib.request
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 
-nltk.data.path.append("c:\users\user\appdata\local\programs\python\python311\lib\site-packages")
+nltk.data.path.append("c:/users/user/appdata/local/programs/python/python311/lib/site-packages")
 
 # Page configuration
 st.set_page_config(
